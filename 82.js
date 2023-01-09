4c5e4a6cf66e27c451841dc24649c0166e050f64
artazeon <artazeon@gmail.com>
2023-01-09 18:00:48 +0300

    freecodecamp 81-83

---
+++ 82.js
@@ -0,0 +1,40 @@
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+
+  if (card > 1 && card < 7) {
+    count++; 
+  }
+
+  else if (card === 10 || card === 'J' || card === 'Q' || card === 'K' || card === 'A') {
+    count--;
+  }
+
+ 
+  
+
+
+
+  if (count > 0) {
+    return count + " Bet";
+    
+  } 
+
+  else {
+    return count + " Hold";
+  }
+
+
+
+  
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A'); 
+
+console.log(count)
+
+// +1+1+0-1-1
+
+// 0 Hold
---
+++ 83.js
@@ -0,0 +1,10 @@
+const myDog = {
+  // Only change code below this line
+
+name: "Yarik",
+legs: 4,
+tails: 1,
+friends: ["People", "Sosidg"]
+
+  // Only change code above this line
+};
