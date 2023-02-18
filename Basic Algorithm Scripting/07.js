2f0e43d01483c46a670dce6fa40a979377bde9ab
artazeon <artazeon@gmail.com>
2023-02-18 22:15:55 +0300

    freecodecamp - Basic Algorithm Scripting - 07

---
+++ Basic Algorithm Scripting/07.js
@@ -0,0 +1,12 @@
+function repeatStringNumTimes(str, num) {
+  let newStr = ''
+  for (let i = 0; i < num; i++){
+    newStr = newStr + str
+    
+  }
+
+  //console.log(newStr)
+  return newStr;
+
+}
+repeatStringNumTimes("abc", 3);
